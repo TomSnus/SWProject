ce3b4fb3dba0d2adec11cb544a1db2fb3fb98303
TomSnus <senteswins@gmail.com>
2017-11-27 16:43:26 +0100

    added Address, Customer, Order, fuel, forwardingComapny, order

---
+++ src/main/java/de/oth/stelzer/swstelzer/entity/OCsingleIdEntity.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.oth.stelzer.swstelzer.entity;
+
+import groovy.transform.EqualsAndHashCode;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Tom
+ */
+@MappedSuperclass
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public abstract class OCsingleIdEntity  implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(
+    strategy=GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof OCsingleIdEntity)) {
+            return false;
+        }
+        OCsingleIdEntity other = (OCsingleIdEntity) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "de.oth.stelzer.swstelzer.entities.OCsingleIdEntity[ id=" + id + " ]";
+    }
+    
+}
